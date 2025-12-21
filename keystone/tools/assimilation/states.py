f2e8706f83a7303e0a3c822e9f4bb787150dd087
David Hooton <david@hooton.org>
2025-12-21 14:06:18 +1100

    chore(states-py): file deleted

--- keystone/tools/assimilation/states.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class AssimilationState(str, Enum):
-    OBSERVED = "observed"
-    CATALOGUED = "catalogued"
-    GOVERNED = "governed"
-    CANONICAL = "canonical"
-    IPBANKED = "ipbanked"
